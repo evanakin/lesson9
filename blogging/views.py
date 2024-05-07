11ee6d41b03538c27fcfd5efe8461aff0aea06fe
evanakin <55139730+evanakin@users.noreply.github.com>
2024-05-06 17:45:28 -0700

    third commit

--- blogging/views.py
+++ blogging/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-
 from blogging.models import Post
-
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
 
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
     context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
-
+    return render(request, 'blogging/list.html', context)
 
 def detail_view(request, post_id):
     published = Post.objects.exclude(published_date__exact=None)
