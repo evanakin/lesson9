a199c0a629bc9a211a2d27b6a1edff71af9e2034
evanakin <55139730+evanakin@users.noreply.github.com>
2024-05-07 13:41:50 -0700

    second commit

--- blogging/views.py
+++ blogging/views.py
@@ -1,19 +1,13 @@
-from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from blogging.models import Post
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class PostListView(ListView):
+    context_object_name = 'posts'
+    queryset = Post.objects.exclude(published_date__isnull=True).order_by('-published_date')
+    template_name = 'blogging/list.html'
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+class PostDetailView(DetailView):
+    context_object_name = 'posts'
+    queryset = Post.objects.exclude(published_date__isnull=True)
+    template_name = 'blogging/detail.html'
