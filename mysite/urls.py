9edf0367bb33b8cc8b5d60e67bb6d68fdd5d91a0
evanakin <55139730+evanakin@users.noreply.github.com>
2024-05-06 17:28:45 -0700

    first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for mysite project.
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+
+
 
 urlpatterns = [
-    path("polling/", include("polling.urls")),
-    path("admin/", admin.site.urls),
+    path('', include('blogging.urls')),
+    path('polling/', include('polling.urls')),
+    path('admin/', admin.site.urls),
+    path('login/', LoginView.as_view(template_name='login.html'), name="login"),
+    path('logout/', LogoutView.as_view(next_page='/'), name="logout"),
 ]
